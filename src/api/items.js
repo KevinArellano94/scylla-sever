f730f1d0742e5db403b2b369c55d287b59a89ef0
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2023-05-24 21:40:23 -0400

    Added dependencies

--- src/api/items.js
+++ src/api/items.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const cassandra = require('cassandra-driver');
 
+const NODE_1 = 'node-0.aws_us_east_1.f8524ca79ecfb383ac83.clusters.scylla.cloud';
+const NODE_2 = 'node-1.aws_us_east_1.f8524ca79ecfb383ac83.clusters.scylla.cloud';
+const NODE_3 = 'node-2.aws_us_east_1.f8524ca79ecfb383ac83.clusters.scylla.cloud';
+const LOCALDATACENTER = 'AWS_US_EAST_1';
+const USERNAME = 'scylla';
+const PASSWORD = 'hIDbWvexa57mu3s';
+const KEYSPACE = 'mykeyspace';
+
 const cluster = new cassandra.Client({
     contactPoints: [
         NODE_1,
